38baf0b3b3b8bec1f57cf442ebeac2e701c68367
rtavaresramos <rtavareskxd99@gmail.com>
2020-05-10 21:05:04 -0300

    First version of the project - static version --Adjusting cards link

--- jquery.js
+++ jquery.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
       console.log(data)
       $.each(data, function(i, data){
         
-        auxCards[i] = '<div class="card--item"><img src="'+ data.url_img +'"alt=""><div class="card--content"><div class="card--text"><h1>'+ data.course_name +'<span>'+ data.course_author+'</span></h1></div><div class="card--price"><h3>R$ '+ data.price +'</h3><div class="button__see--more"><a href="'+ data.url_affiliate +'">Ver curso</a></div></div></div></div></div>'
+        auxCards[i] = '<div class="card--item"><img src="'+ data.url_img +'"alt=""><div class="card--content"><div class="card--text"><h1>'+ data.course_name +'<span>'+ data.course_author+'</span></h1></div><div class="card--price"><h3>R$ '+ data.price +'</h3><div class="button__see--more"><a href="'+ data.url_affiliate +'" target="_blank">Ver curso</a></div></div></div></div></div>'
         container.append( auxCards[i])
         var i 
       return auxCards 
